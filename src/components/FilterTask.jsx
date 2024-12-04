b64493694818914ad31c1460c5738daf54bd24a4
Vadivel-Babu <vadivelbabu1996@gmail.com>
2024-12-04 13:07:57 +0530

    drag and drop added

--- src/components/FilterTask.jsx
+++ src/components/FilterTask.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const FilterTask = ({ handleFilter, handleQuery }) => {
   return (
-    <div className="mt-2 flex shadow-lg p-2 rounded-lg gap-2 justify-between flex-wrap w-full md:w-[550px]">
+    <div className="mt-2 flex shadow-lg p-2 rounded-lg gap-2 justify-between flex-wrap w-full md:w-[650px]">
       <Input
         placeholder="search by title"
         onChange={(e) => handleQuery(e.target.value)}
--- src/pages/AllTaskPage.jsx
+++ src/pages/AllTaskPage.jsx
@@ -1,6 +1,5 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
-import { Button } from "antd";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import BackButton from "../components/BackButton";
 import FilterTask from "../components/FilterTask";
@@ -10,12 +9,13 @@ const AllTaskPage = () => {
   const tasks = useSelector((state) => state.tasks);
   const [filter, setFilter] = useState("all"); // State to store the current filter
   const [searchQuery, setSearchQuery] = useState("");
+  const [reorderTasks, setReorderTasks] = useState([]);
 
   // Get the current date
   const currentDate = new Date();
 
   // Filter tasks based on the selected filter
-  const filteredTasks = tasks
+  const filteredTasks = reorderTasks
     .filter((task) => {
       if (filter === "all") return true;
       if (filter === "completed") return task.isCompleted === true;
@@ -28,21 +28,56 @@ const AllTaskPage = () => {
       return task.title.toLowerCase().includes(searchQuery.toLowerCase());
     });
 
+  const [draggedItem, setDraggedItem] = useState(null);
+
+  const handleDragStart = (index) => {
+    setDraggedItem(index);
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault(); // Allow dropping
+  };
+
+  const handleDrop = (index) => {
+    if (draggedItem === index) return;
+
+    const updatedTasks = [...reorderTasks];
+    const [movedTask] = updatedTasks.splice(draggedItem, 1);
+    updatedTasks.splice(index, 0, movedTask);
+
+    setDraggedItem(index);
+    setReorderTasks(updatedTasks);
+  };
+
+  useEffect(() => {
+    //set task to display reorder list
+    setReorderTasks(tasks);
+  }, []);
+
   return (
     <div className="container-lg p-3 w-full">
       <BackButton />
       <FilterTask handleFilter={setFilter} handleQuery={setSearchQuery} />
       <div className="mt-2 flex gap-2 flex-wrap">
-        {filteredTasks.length === 0 ? (
+        {filteredTasks.length === 0 && (
           <p className="text-3xl text-center font-bold">No Task Found</p>
-        ) : (
-          filteredTasks.map((task) => (
-            <div className="shadow-lg p-2 w-[200px]" key={task.id}>
+        )}
+
+        {filteredTasks.length > 0 &&
+          filteredTasks.map((task, index) => (
+            <div
+              key={task.id}
+              draggable
+              onDragStart={() => handleDragStart(index)} // Triggered when drag starts
+              onDragOver={handleDragOver} // Triggered when dragging over a valid target
+              onDrop={() => handleDrop(index)}
+              className="shadow-lg p-2 w-[200px] bg-white mt-1 cursor-grab"
+            >
               <h1 className="text-xl font-medium">{task.title}</h1>
               <p className="text-lg">{task.content}</p>
               <Link
                 to={`/task/${task.id}`}
-                className="flex gap-1 items-center text-blue font-medium"
+                className="flex gap-1 items-center text-blue font-medium cursor-pointer"
               >
                 see more{" "}
                 <Icon
@@ -51,8 +86,7 @@ const AllTaskPage = () => {
                 />
               </Link>
             </div>
-          ))
-        )}
+          ))}
       </div>
     </div>
   );
